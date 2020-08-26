22e48c50ce4aefdfa57b91bb2993a91a179bb26f
AMAIISDOM\snegi <suraj.negi@amadeus.com>
2020-08-26 23:22:03 +0530

    added types

---
+++ JS/1.types/1.Objects.js
@@ -0,0 +1,26 @@
+/*
+    -The Object class represents one of JavaScript's data types.
+    -It is used to store various keyed collections and more complex entities
+
+    -- Nearly all objects in JavaScript are instances of Object;
+    -- a typical object inherits properties (including methods) from Object.prototype, although these properties may be shadowed (a.k.a. overridden).
+    -- However, an Object may be deliberately created for which this is not true (e.g. by Object.create(null)),
+    -- or it may be altered so that this is no longer true (e.g. with Object.setPrototypeOf).
+
+    - Ways to create an Object:
+        -- var a = {};
+        -- var a = new Object();
+*/
+
+
+/*
+    -Object.assign()
+        -- copies all enumerable own properties from one or more source objects to a target object. It returns the target object.
+
+    - Object.assign(target, ...sources)
+
+    const obj = { a: 1 };
+    const copy = Object.assign({}, obj);
+    console.log(copy); // { a: 1 }
+*/
+
---
+++ JS/1.types/2.Array.js
@@ -0,0 +1,67 @@
+/*  
+    Array is an ordered collection.
+        - Arrays cannot use strings as element indexes but use integers.
+
+    let fruits = ['Apple', 'Banana']
+
+*/
+
+// Methods in Array.prototype:
+
+/*
+    Array.from() lets you create Arrays from:
+        - array-like objects (objects with a length property and indexed elements); or
+        - iterable objects (objects such as Map and Set).
+*/
+console.log(Array.from('foo'));
+// expected output: Array ["f", "o", "o"]
+
+console.log(Array.from([1, 2, 3], x => x + x));
+// expected output: Array [2, 4, 6]
+
+
+/*
+    The Array.isArray() method determines whether the passed value is an Array.
+*/
+console.log(Array.isArray([1, 2, 3]))   // true
+console.log(Array.isArray({ foo: 123 })); // false
+console.log(Array.isArray('foobar'))    // false
+console.log(Array.isArray(undefined))   // false
+
+/*  
+    Array.of() method creates a new Array instance from a variable number of arguments, regardless of number or type of the arguments.
+*/
+Array.of(7);       // [7] 
+Array.of(1, 2, 3); // [1, 2, 3]
+
+Array(7);          // array of 7 empty slots
+Array(1, 2, 3);    // [1, 2, 3]
+
+/*
+    The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+*/
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+/*
+    The copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length.    
+
+*/
+const arrayCopyWithin = ['a', 'b', 'c', 'd', 'e'];
+
+// copy to index 0 the element at index 3
+console.log(arrayCopyWithin.copyWithin(0, 3, 4));
+// expected output: Array ["d", "b", "c", "d", "e"]
+
+// copy to index 1 all elements from index 3 to the end
+console.log(arrayCopyWithin.copyWithin(1, 3));
+// expected output: Array ["d", "d", "e", "d", "e"]
+
+
+/*
+    entries() method returns a new Array Iterator object that contains the key/value pairs for each index in the array.
+*/
